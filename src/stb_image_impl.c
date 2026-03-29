669f59675ebf553c9dffdf947ace68088faa12bf
lilbenji03 <walibenjamin03@gmail.com>
2026-03-29 21:20:09 +0300

    fixed bugs

--- src/stb_image_impl.c
+++ src/stb_image_impl.c
@@ -1,8 +1,8 @@
 /*
- * stb_image_resize2_impl.c
- * Compile stb_image_resize2 once here.
- * Download stb_image_resize2.h from:
- * https://raw.githubusercontent.com/nothings/stb/master/stb_image_resize2.h
+ * stb_image_impl.c
+ * Compile stb_image once here so every other .c file can just #include "stb_image.h"
+ * Download stb_image.h from: https://raw.githubusercontent.com/nothings/stb/master/stb_image.h
  */
-#define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include "stb_image_resize2.h"
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_WINDOWS_UTF8        /* enable UTF-8 path support on Windows */
+#include "stb_image.h"
